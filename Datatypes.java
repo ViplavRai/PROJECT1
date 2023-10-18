c7b0ba77d30751c814c6f828583dec1c0ed7fb5f
ViplavRai <147581461+ViplavRai@users.noreply.github.com>
2023-10-19 00:38:26 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,21 @@
+package day1;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		
+		int   a=110;
+		short b=22;
+		long  l=22l;
+		float f= 23.3f;
+		double d=22.33d;
+		char c='a';
+		byte d1=33;
+		boolean r= true;
+		
+		
+		
+
+	}
+
+}
